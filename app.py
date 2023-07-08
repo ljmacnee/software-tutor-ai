6c40dbdc7133b5a70e269e9982d1f10e0e6dacf6
ljmacnee <l.macnee@outlook.com>
2023-07-08 18:08:43 +1000

    updated for hsc software

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import os
+from io import BytesIO
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
@@ -69,35 +71,55 @@ def main():
     st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
-
+   
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+        
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
+    st.header("Chat AI HSC Software Tutor :books:")
+    user_question = st.text_input("Ask a question about your course:")
     if user_question:
         handle_userinput(user_question)
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
+    # Specify the directory path
+    dir_path = "hscsoftwarepdf"
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+    # Get a list of all files in the directory
+    files = os.listdir(dir_path)
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+    # List to store file contents as BytesIO objects
+    file_contents = []
+
+    # Iterate over each file in the directory
+    for file in files:
+        # Read the file
+        file_path = os.path.join(dir_path, file)
+        with open(file_path, "rb") as f:
+            data = f.read()
+        
+        # Create BytesIO object from file contents
+        file_io = BytesIO(data)
+        
+        # Add BytesIO object to the list
+        file_contents.append(file_io)
+
+    # get pdf text
+    raw_text = get_pdf_text(file_contents)
+
+    # get the text chunks
+    text_chunks = get_text_chunks(raw_text)
+
+    # create vector store
+    vectorstore = get_vectorstore(text_chunks)
+
+    
+
+    # create conversation chain
+    st.session_state.conversation = get_conversation_chain(
+        vectorstore)
 
 
 if __name__ == '__main__':
